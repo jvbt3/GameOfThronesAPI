6a2e17168486601d316e8301972c20482ca19ec3
jvbt3 <joaovitormartins077@gmail.com>
2024-05-24 19:59:42 -0300

    correção findall logs

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import {LogsModule} from "../logs/logs.module";
-import {MongooseModule} from "@nestjs/mongoose";
-import {User, UserSchema} from "./schema/users.schema";
+import { LogsModule } from '../logs/logs.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema/users.schema';
 
 @Module({
-  imports: [LogsModule, MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    LogsModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
